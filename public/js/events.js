d0e7cc8d514fac97f48dee8515ca2ce26d4aca95
ManeRomero <algoritmo717@gmail.com>
2019-10-21 22:46:59 +0200

    jquery : multiple image preview on upload

--- public/js/events.js
+++ public/js/events.js
@@ -14,3 +14,27 @@ $("[id*=del]").click((e) => {
         })
     }
 })
+
+$(function() {
+    // Multiple images preview in browser
+    var imagesPreview = function(input, placeToInsertImagePreview) {
+
+        if (input.files) {
+            var filesAmount = input.files.length;
+
+            for (i = 0; i < filesAmount; i++) {
+                var reader = new FileReader();
+
+                reader.onload = function(event) {
+                    $($.parseHTML('<img>')).attr('src', event.target.result).attr('class', 'img-fluid mx-auto p-4').appendTo(placeToInsertImagePreview);
+                }
+
+                reader.readAsDataURL(input.files[i]);
+            }
+        }
+    };
+
+    $('#inputImages').on('change', function() {
+        imagesPreview(this, 'div.galeria');
+    });
+});
