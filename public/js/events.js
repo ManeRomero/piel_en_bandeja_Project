9ce98142cc37eda815f0dd952dcdcecf85a760ad
ManeRomero <algoritmo717@gmail.com>
2019-10-27 20:49:55 +0100

    confirmation labelling at clear items fixed

--- public/js/events.js
+++ public/js/events.js
@@ -1,17 +1,8 @@
 $("[id*=del]").click((e) => {
     let confirmation = confirm('Estas a punto de eliminar una bandeja. ¿Seguro?')
-
-    if (confirmation) {
-        let bandejaId = e.originalEvent.toElement.id
-        let id = bandejaId.substr(bandejaId.lastIndexOf('-') + 1, bandejaId.length)
-
-        $.ajax({
-            url: '/admin/bandejaClear/' + id,
-            type: 'DELETE',
-            success: function (result) {
-                console.log(result);
-            }
-        })
+    if (!confirmation) {
+        e.preventDefault()
+        e.stopPropagation()
     }
 })
 
